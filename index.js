bf9181178b06a3338baaff454280b87e2eef6a66
DOTL-Amir-R <raeisi.amirrr@gmail.com>
2022-07-06 19:20:12 +0430

    HWJS03V03

--- index.js
+++ index.js
@@ -1,77 +1,170 @@
-const a = +prompt('please give us the first number');
-const b = +prompt('please give us the second number');
-const c = prompt('please give us the action for calc');
-let answer=null;
-// this is my first way fo making a
-const myCalc = {
-    Calc(a,b,c){
-        switch(c){
-            case `+` :
-                answer=a+b;
-                alert(answer);
-                console.log(answer);
+//this is my main way of making calc
+const myCalculator = {
+    result:0,
+    firstNumber: +prompt('please give us the first number'),
+    secondNumber: +prompt('please give us the second number'),
+    action: prompt('please give us the action for calc'),
+    sum(){
+        this.result=this.firstNumber+this.secondNumber;
+        alert(` this is your result ${this.result} for first number ${this.firstNumber} + ${this.secondNumber}`);
+        console.log(` this is your result ${this.result} for first number ${this.firstNumber} + ${this.secondNumber}`);
+    },
+    mines(){
+        this.result=this.firstNumber-this.secondNumber;
+        alert(` this is your result ${this.result} for first number ${this.firstNumber} - ${this.secondNumber}`);
+        console.log(` this is your result ${this.result} for first number ${this.firstNumber} - ${this.secondNumber}`);
+    },
+    division(){
+        this.result=this.firstNumber/this.secondNumber;
+        alert(` this is your result ${this.result} for first number ${this.firstNumber} / ${this.secondNumber}`);
+        console.log(` this is your result ${this.result} for first number ${this.firstNumber} / ${this.secondNumber}`);
+    },
+    multi(){
+        this.result=this.firstNumber*this.secondNumber;
+        alert(` this is your result ${this.result} for first number ${this.firstNumber} * ${this.secondNumber}`);
+        console.log(` this is your result ${this.result} for first number ${this.firstNumber} * ${this.secondNumber}`);
+    },
+    showTheResult(){
+        switch(this.action){
+            case '+':
+                this.sum()
                 break;
-            case `-` :
-                 answer=a-b;
-                 alert(answer);
-                 console.log(answer);
-                 break;
-            case `/` :
-                answer=a/b;
-                alert(answer);
-                console.log(answer);
+            case '-':
+                this.mines()
                 break;
-            case `*` :
-                answer=a*b;
-                alert(answer);
-                console.log(answer);
+            case '*':
+                this.multi()
                 break;
-            default:
-                console.log('Error');
+            case '/':
+                this.division()
+                break;
         }
-    }
-};
-myCalc.Calc(a,b,c)
+    },
+}
+
+myCalculator.showTheResult()
+
+
+
+
+// // this is my first way fo making a calculater
+
+
+// const firstNumber = +prompt('please give us the first number');
+// const secondNumber = +prompt('please give us the second number');
+// const action = prompt('please give us the action for calc');
+// let answer=null;
+
+// const myCalc = {
+//     Calc(firstNumber,secondNumber,action){
+//         switch(action){
+//             case `+` :
+//                 answer=firstNumber+secondNumber;
+//                 alert(answer);
+//                 console.log(answer);
+//                 break;
+//             case `-` :
+//                  answer=firstNumber-secondNumber;
+//                  alert(answer);
+//                  console.log(answer);
+//                  break;
+//             case `/` :
+//                 answer=firstNumber/secondNumber;
+//                 alert(answer);
+//                 console.log(answer);
+//                 break;
+//             case `*` :
+//                 answer=firstNumber*secondNumber;
+//                 alert(answer);
+//                 console.log(answer);
+//                 break;
+//             default:
+//                 console.log('Error');
+//         }
+//     }
+// };
+// myCalc.Calc(firstNumber,secondNumber,action)
+
+
+
+
+
+
+
+
+// this is my second way fo making a calculater
+
 
 // const mySecondCalc = {
-//     sumFunction(a,b){
-//         answer=a+b
+//     sumFunction(firstNumber,secondNumber){
+//         answer=firstNumber+secondNumber
 //         alert(answer);
 //         console.log(answer);
 //     },
-//     minesFunction(a,b){
-//         answer=a-b
+//     minesFunction(firstNumber,secondNumber){
+//         answer=firstNumber-secondNumber
 //         alert(answer);
 //         console.log(answer);
 //     },
-//     multiFunction(a,b){
-//         answer=a*b
+//     multiFunction(firstNumber,secondNumber){
+//         answer=firstNumber*secondNumber
 //         alert(answer);
 //         console.log(answer);
 //     },
-//     dividFunction(a,b){
-//         answer=a/b
+//     dividFunction(firstNumber,secondNumber){
+//         answer=firstNumber/secondNumber
 //         alert(answer);
 //         console.log(answer);
 //     },
 // }
 
-// switch(c){
+// switch(action){
 //     case '+':
-//         mySecondCalc.sumFunction(a,b);
+//         mySecondCalc.sumFunction(firstNumber,secondNumber);
 //         break;
 //     case '-':
-//         mySecondCalc.minesFunction(a,b);
+//         mySecondCalc.minesFunction(firstNumber,secondNumber);
 //         break;
 //     case '*':
-//         mySecondCalc.multiFunction(a,b);
+//         mySecondCalc.multiFunction(firstNumber,secondNumber);
 //         break;
 //     case '/':
-//         mySecondCalc.dividFunction(a,b);
+//         mySecondCalc.dividFunction(firstNumber,secondNumber);
 //         break;
 //     default:
 //         console.log('Error');
 //         alert('Error');
     
 // }
+
+
+
+
+//this is my third way
+
+
+
+// const myCalculator = {
+//     result:0,
+//     firstNumber:2,
+//     secondNumber:1,
+//     sum(){
+//         this.result=this.firstNumber+this.secondNumber;
+//         console.log(this.result)
+//     },
+//     mines(){
+//         this.result=this.firstNumber-this.secondNumber;
+//         console.log(this.result)
+//     },
+//     division(){
+//         this.result=this.firstNumber/this.secondNumber;
+//         console.log(this.result)
+//     },
+//     multi(){
+//         this.result=this.firstNumber*this.secondNumber;
+//         console.log(this.result)
+//     },
+// }
+// myCalculator.mines()
+
 
