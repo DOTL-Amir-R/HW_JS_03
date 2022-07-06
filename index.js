cb500066ac9789abdfe85f34b4c5ee3ba204a959
DOTL-Amir-R <raeisi.amirrr@gmail.com>
2022-07-06 13:24:04 +0430

    HWJS03V02

--- index.js
+++ index.js
@@ -2,24 +2,28 @@ const a = +prompt('please give us the first number');
 const b = +prompt('please give us the second number');
 const c = prompt('please give us the action for calc');
 let answer=null;
-
+// this is my first way fo making a
 const myCalc = {
     Calc(a,b,c){
         switch(c){
             case `+` :
                 answer=a+b;
                 alert(answer);
+                console.log(answer);
                 break;
             case `-` :
                  answer=a-b;
+                 alert(answer);
                  console.log(answer);
                  break;
             case `/` :
                 answer=a/b;
+                alert(answer);
                 console.log(answer);
                 break;
             case `*` :
                 answer=a*b;
+                alert(answer);
                 console.log(answer);
                 break;
             default:
@@ -29,4 +33,45 @@ const myCalc = {
 };
 myCalc.Calc(a,b,c)
 
+// const mySecondCalc = {
+//     sumFunction(a,b){
+//         answer=a+b
+//         alert(answer);
+//         console.log(answer);
+//     },
+//     minesFunction(a,b){
+//         answer=a-b
+//         alert(answer);
+//         console.log(answer);
+//     },
+//     multiFunction(a,b){
+//         answer=a*b
+//         alert(answer);
+//         console.log(answer);
+//     },
+//     dividFunction(a,b){
+//         answer=a/b
+//         alert(answer);
+//         console.log(answer);
+//     },
+// }
+
+// switch(c){
+//     case '+':
+//         mySecondCalc.sumFunction(a,b);
+//         break;
+//     case '-':
+//         mySecondCalc.minesFunction(a,b);
+//         break;
+//     case '*':
+//         mySecondCalc.multiFunction(a,b);
+//         break;
+//     case '/':
+//         mySecondCalc.dividFunction(a,b);
+//         break;
+//     default:
+//         console.log('Error');
+//         alert('Error');
+    
+// }
 
